9b0e1a8f0e3040aa773d2df50fe054c432597680
cmpsgabi <cmpsgabi@gmail.com>
2025-01-07 16:46:14 -0300

    mainscreenworks

--- spotify-music-quiz/src/app/app.routes.ts
+++ spotify-music-quiz/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainscreenComponent } from './features/mainscreen/mainscreen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'mainscreen',
+        component: Mainscreen
+    }
+];
--- spotify-music-quiz/src/app/features/mainscreen/mainscreen.component.ts
+++ spotify-music-quiz/src/app/features/mainscreen/mainscreen.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-mainscreen',
+  selector: 'Mainscreen',
   standalone: true,
   imports: [],
-  templateUrl: './mainscreen.component.html',
-  styleUrl: './mainscreen.component.css'
+  templateUrl: './features/mainscreen.component.html',
+  styleUrl: './features/mainscreen.component.css'
 })
 export class MainscreenComponent {
 
